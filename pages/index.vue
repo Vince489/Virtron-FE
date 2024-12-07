35c2c442ddec7ca6e12ad61232756962566a3eba
Vince489 <vincentcornelius489@gmail.com>
2024-12-06 22:23:58 -0500

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="text-blue-600 text-2xl">Welcome to Virtron</h1>
+  </div>
+
+  <div>
+    <Button class="bg-blue-400">Click me</Button>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
